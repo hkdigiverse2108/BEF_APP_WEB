f888b7e0d525bad14185a6d8741d1cf398500ce9
shakilhkdigiverse-alt <shakilhkdigiverse@gmail.com>
2025-12-23 17:46:48 +0530

    Exam Timer Added

--- src/Layout/Footer.tsx
+++ src/Layout/Footer.tsx
@@ -4,26 +4,28 @@ import { HK_DIGIVERSE_URL } from "../Constants";
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-white flex flex-wrap justify-center md:justify-between items-center p-5 gap-3 px-9">
-      {/* Social Icons */}
-      <div className="flex gap-3 mt-4">
-        {SOCIAL_MEDIA?.map((item) => {
-          const Icon = item?.icon;
-          return (
-            <Link key={item?.title} to={item?.link} className="w-9 h-9 rounded-full bg-white/20 flex items-center justify-center hover:bg-white hover:text-orange-500 transition">
-              <Icon />
-            </Link>
-          );
-        })}
-      </div>
+    <div className="bg-black  text-white">
+      <footer className="container  flex flex-wrap justify-center md:justify-between items-center p-5 gap-3 px-9">
+        {/* Social Icons */}
+        <div className="flex gap-3 mt-4">
+          {SOCIAL_MEDIA?.map((item) => {
+            const Icon = item?.icon;
+            return (
+              <Link key={item?.title} to={item?.link} className="w-9 h-9 rounded-full bg-white/20 flex items-center justify-center hover:bg-white hover:text-orange-500 transition">
+                <Icon />
+              </Link>
+            );
+          })}
+        </div>
 
-      <div className="text-center text-base">
-        © Copyrights {new Date().getFullYear()} All rights reserved by
-        <Link target="_blank" to={HK_DIGIVERSE_URL} className="font-medium underline ps-1">
-          HK DigiVerse LLP
-        </Link>
-      </div>
-    </footer>
+        <div className="text-center text-base">
+          © Copyrights {new Date().getFullYear()} All rights reserved by
+          <Link target="_blank" to={HK_DIGIVERSE_URL} className="font-medium underline ps-1">
+            HK DigiVerse LLP
+          </Link>
+        </div>
+      </footer>
+    </div>
   );
 };
 
--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -8,17 +8,15 @@ import { useAppDispatch, useAppSelector } from "../Store/hooks";
 import { setMenuDrawer, setNavMenuDrawer } from "../Store/Slices/DrawerSlice";
 import MenuDrawer from "./MenuDrawer";
 import NavMenuDrawer from "./NavMenuDrawer";
-import {
-  setworkshopLoading,
-  setWorkshops,
-} from "../Store/Slices/WorkshopSlice";
+import { setworkshopLoading, setWorkshops } from "../Store/Slices/WorkshopSlice";
 import { getHeaderItems } from "../Utils/GetHeaderItems";
+import ExamTimer from "../Components/Common/ExamTimer";
 
 const Header = () => {
   const dispatch = useAppDispatch();
   const [isScrolled, setIsScrolled] = useState(false);
 
-  const { genderWiseProfileImage ,user} = useAppSelector((state) => state.auth);
+  const { genderWiseProfileImage, user } = useAppSelector((state) => state.auth);
 
   const { data } = useGetApiQuery({ url: `${URL_KEYS.USER.ID}${user?._id}` });
   const UserData = data?.data;
@@ -57,75 +55,43 @@ const Header = () => {
 
   const currentUser = UserData || user;
 
-  const ProfileImage = currentUser?.profileImage
-    ? currentUser?.profileImage
-    : genderWiseProfileImage;
+  const ProfileImage = currentUser?.profileImage ? currentUser?.profileImage : genderWiseProfileImage;
 
   return (
     <>
       <div className="sticky top-0 w-full z-50">
-        <header
-          className={`min-h-[60px] tracking-wide relative z-50 ${
-            isScrolled ? "bg-white/90 backdrop-blur-md" : ""
-          }`}
-        >
+        {/* Timer For Exam */}
+        <ExamTimer />
+        <header className={`container min-h-[60px] tracking-wide relative z-50 ${isScrolled ? "bg-white/90 backdrop-blur-md" : ""}`}>
           <div className="flex flex-wrap items-center justify-between max-sm:px-2 py-3 px-4 sm:px-10 2xl:gap-y-4 gap-y-6 gap-x-4">
-            <Link
-              to={ROUTES.HOME}
-              className="flex max-sm:gap-1.5 gap-2.5 items-center"
-            >
+            <Link to={ROUTES.HOME} className="flex max-sm:gap-1.5 gap-2.5 items-center">
               <figure className="w-9 sm:w-12 md:w-16 h-full">
-                <img
-                  src={`${ImagePath}logo/Logo.png`}
-                  alt="BEF-Logo"
-                  className="w-full h-full object-contain"
-                />
+                <img src={`${ImagePath}logo/Logo.png`} alt="BEF-Logo" className="w-full h-full object-contain" />
               </figure>
               <section className="flex flex-col justify-center">
-                <h1 className="text-[10px] md:text-lg text-primary font-bold sm:font-extrabold">
-                  Bharat Exam Fest
-                </h1>
-                <p className="text-[10px] md:text-sm font-medium text-success ">
-                  Learn & Earn
-                </p>
+                <h1 className="text-[10px] md:text-lg text-primary font-bold sm:font-extrabold">Bharat Exam Fest</h1>
+                <p className="text-[10px] md:text-sm font-medium text-success ">Learn & Earn</p>
               </section>
             </Link>
-            <div
-              id="collapseMenu"
-              className="max-2xl:hidden 2xl:flex! 2xl:items-center max-2xl:before:fixed max-2xl:before:bg-black max-2xl:before:opacity-40 max-2xl:before:inset-0 max-2xl:before:z-50"
-            >
+            <div id="collapseMenu" className="max-2xl:hidden 2xl:flex! 2xl:items-center max-2xl:before:fixed max-2xl:before:bg-black max-2xl:before:opacity-40 max-2xl:before:inset-0 max-2xl:before:z-50">
               <ul className="2xl:flex! 2xl:gap-x-10 max-2xl:space-y-3 max-2xl:fixed max-2xl:bg-theme-bg max-2xl:w-1/2 max-2xl:min-w-[300px] max-2xl:top-0 max-2xl:left-0 max-2xl:px-10 max-2xl:py-4 max-2xl:h-full max-2xl:shadow-md max-2xl:overflow-auto z-50">
                 <li className="mb-6 hidden max-2xl:block">
                   <div className="flex justify-between items-center">
                     <div className="flex gap-4 items-center">
                       <figure className="w-12 sm:w-18 h-full">
-                        <img
-                          src={`${ImagePath}logo/Logo.png`}
-                          alt="BEF-Logo"
-                          className="w-full h-full object-contain"
-                        />
+                        <img src={`${ImagePath}logo/Logo.png`} alt="BEF-Logo" className="w-full h-full object-contain" />
                       </figure>
                       <section className="flex flex-col justify-center max-md:hidden">
-                        <h1 className="text-sm sm:text-xl text-primary font-bold">
-                          Bharat Exam Fest
-                        </h1>
-                        <p className="text-xs sm:text-sm text-success font-medium">
-                          Learn & Earn
-                        </p>
+                        <h1 className="text-sm sm:text-xl text-primary font-bold">Bharat Exam Fest</h1>
+                        <p className="text-xs sm:text-sm text-success font-medium">Learn & Earn</p>
                       </section>
                     </div>
                   </div>
                 </li>
                 <ul className="flex gap-8">
                   {HeaderMenu?.map((item, index) => (
-                    <li
-                      key={index}
-                      className="max-2xl:py-3  relative hover:after:absolute after:bg-theme! after:w-0 hover:after:w-full hover:after:h-0.5 after:block after:-bottom-1 2xl:after:-bottom-4 after:transition-all after:duration-300"
-                    >
-                      <NavLink
-                        to={item.link}
-                        className="block text-lg text-base capitalize "
-                      >
+                    <li key={index} className="max-2xl:py-3  relative hover:after:absolute after:bg-theme! after:w-0 hover:after:w-full hover:after:h-0.5 after:block after:-bottom-1 2xl:after:-bottom-4 after:transition-all after:duration-300">
+                      <NavLink to={item.link} className="block text-lg text-base capitalize ">
                         {item.text}
                       </NavLink>
                     </li>
@@ -143,32 +109,19 @@ const Header = () => {
                   </div>
                 </li>
                 <li className="group relative py-1 px-1.5 sm:px-2.5 flex justify-between items-center rounded-xl h-10 sm:h-12 bg-input-box">
-                  <div
-                    className="flex justify-between items-center gap-3"
-                    onClick={() => dispatch(setMenuDrawer())}
-                  >
-                    <img
-                      src={ProfileImage}
-                      alt="profile"
-                      className="w-10 h-10 rounded-xl"
-                    />
+                  <div className="flex justify-between items-center gap-3" onClick={() => dispatch(setMenuDrawer())}>
+                    <img src={ProfileImage} alt="profile" className="w-10 h-10 rounded-xl" />
                     <div className="flex-1 hidden max-2xl:max-w-35 sm:block max-w-28 truncate">
                       <span className="text-md font-semibold capitalize  ">
                         {UserData?.firstName} {UserData?.lastName}
                       </span>
-                      <p className="capitalize flex text-xs">
-                        {UserData?.userType === "admin" ? "Admin" : "Student"}
-                      </p>
+                      <p className="capitalize flex text-xs">{UserData?.userType === "admin" ? "Admin" : "Student"}</p>
                     </div>
                     <IoIosArrowDown className="hidden sm:block " />
                   </div>
                 </li>
               </ul>
-              <button
-                id="toggleOpen"
-                onClick={() => dispatch(setNavMenuDrawer(true))}
-                className="2xl:hidden ml-2 cursor-pointer p-1 flex justify-center items-center rounded-xl w-10 sm:w-12 h-10 sm:h-12 bg-input-box"
-              >
+              <button id="toggleOpen" onClick={() => dispatch(setNavMenuDrawer(true))} className="2xl:hidden ml-2 cursor-pointer p-1 flex justify-center items-center rounded-xl w-10 sm:w-12 h-10 sm:h-12 bg-input-box">
                 <HiOutlineBars3BottomRight className="text-xl sm:text-2xl" />
               </button>
             </div>
